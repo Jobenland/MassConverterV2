d6109f238613cfc8036d7b9ac75bf6a40861b4c1
Jonathan Obenland <jobenland1@gmail.com>
2021-06-23 19:58:23 -0400

    Added file sorting

---
+++ helper_functions/filesorter.js
@@ -0,0 +1,48 @@
+const fs = require("fs")
+const lineReader = require("line-reader")
+
+function sortfilesbyexperiment(folders, _callback){
+    let sortedFolders = {}
+    foldersParsed = 0
+    folders.forEach(folder => {
+        foldersParsed++
+        let experimentFiles = {
+            impedance: [],
+            galvanodynamic: [],
+            galvanostatic: [],
+            potentiostatic: [],
+            osv: []
+        }
+        fs.readdir(`${folder}\\Run01`, (err, files) => {
+            if (err) {
+                console.log(err)
+            } else {
+                files.forEach(file => {
+                    lineReader.eachLine(`${folder}\\Run01\\${file}`, function(line, last) {
+                        if (line.includes("Exp Title: ")) {
+                            if (line.includes("Impedance")) {
+                                experimentFiles['impedance'].push(`${folder}\\Run01\\${file}`)
+                            } else if (line.includes("Galvanodynamic")) {
+                                experimentFiles['galvanodynamic'].push(`${folder}\\Run01\\${file}`)
+                            } else if (line.includes("Galvanostatic")) {
+                                experimentFiles['galvanostatic'].push(`${folder}\\Run01\\${file}`)
+                            } else if (line.includes("Potentiostatic")) {
+                                experimentFiles['potentiostatic'].push(`${folder}\\Run01\\${file}`)
+                            } else if (line.includes("Open Circuit")) {
+                                experimentFiles['osv'].push(`${folder}\\Run01\\${file}`)
+                            }
+                        }
+                        if (last) {
+                            sortedFolders[folder] = experimentFiles
+                            if (foldersParsed == folders.length) {
+                                _callback(sortedFolders)
+                            } 
+                        }
+                    })
+                })
+            }
+        }) 
+    })
+}
+
+module.exports = { sortfilesbyexperiment }
--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const colors = require("colors")
 const filehandler = require("./helper_functions/filehandler")
+const filesorter = require("./helper_functions/filesorter")
 
+//Finds the MDAT files, converts them to zip, and then extracts them to a folder with the name name as the file
+//This function returns the list of folders to perform analysis on
 function getFolders() {
     return new Promise(function (resolve, reject) {
         filehandler.manipulateFiles(process.argv[2], function(foldersCreated) {
             resolve(foldersCreated)
+        })
+    })
+}
+
+function sortFiles(folders) {
+    return new Promise(function (resolve, reject) {
+        filesorter.sortfilesbyexperiment(folders, function(sortedFiles) {
+            resolve(sortedFiles)
         })
     })
 }
@@ -14,12 +25,16 @@ console.log("Maryland Energy Innovation Institute".blue)
 let arguments = process.argv
 console.log(arguments)
 if (arguments.length > 2) {
-    let foldersCreated = []
+    let foldersCreated = ['C:\\Users\\Jonathan\\MassConverterV2\\test\\EIS_OCV_IV_600_aging_100sccmO2']
+    let experimentTypes = {}
     let mainFunction = async (_) => {
-        await getFolders().then(function (result) {
-            foldersCreated = result
+        // await getFolders().then(function (result) {
+        //     foldersCreated = result
+        // })
+        await sortFiles(foldersCreated).then(function (result) {
+            console.log(result)
         })
-        console.log(foldersCreated)
+
     }
     mainFunction()
 } else {
