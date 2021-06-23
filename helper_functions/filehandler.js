e0518295c496688cc5d2e02379a86ea69c6404a9
Jonathan Obenland <jobenland1@gmail.com>
2021-06-23 19:06:59 -0400

    Added sync and async functions to await callback before procedding

--- helper_functions/filehandler.js
+++ helper_functions/filehandler.js
@@ -4,25 +4,33 @@ const decompress = require("decompress")
 const path = require("path")
 const fs = require("fs")
 
-function manipulateFiles(initialPath){
+function manipulateFiles(initialPath, _callback){
     fs.readdir(initialPath, (err, files) => {
         if (err) {
             console.log(err)
             console.log("Error! Path does not exist. Enter a valid path".red)
         } else {
             console.log(`Successfully initialized working directory at ${initialPath}`.green)
+            foldersCreated = []
+            let filesChecked = 0
             files.forEach(file => {
+                filesChecked++
                 if (path.extname(file) == ".mdat") {
                     console.log("located MDAT file...")
                     let newPath = replaceExt(`${initialPath}\\${file}`, '.zip');
                     fs.rename(`${initialPath}\\${file}`, newPath, function(err) {
                         if ( err ) console.log('ERROR: ' + err);
                     });
-                    decompress(newPath, `${initialPath}\\${file.replace(/\.[^/.]+$/, "")}`).then(files => {
+                    decompress(newPath, `${initialPath}\\${file.replace(/\.[^/.]+$/, "")}`).then(decompressedFiles => {
                         console.log("Converted MDAT file to ZIP and decompressed");
+                        foldersCreated.push(`${initialPath}\\${file.replace(/\.[^/.]+$/, "")}`)
+                        if (filesChecked === files.length) {
+                            console.log(`Generated ${foldersCreated.length} folder`)
+                            _callback(foldersCreated)
+                        }
                     });
                 }
-        });
+            });
         }
     })
 }
--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const colors = require("colors")
 const filehandler = require("./helper_functions/filehandler")
 
+function getFolders() {
+    return new Promise(function (resolve, reject) {
+        filehandler.manipulateFiles(process.argv[2], function(foldersCreated) {
+            resolve(foldersCreated)
+        })
+    })
+}
+
 console.log("Mass Converter v2".yellow)
 console.log("Maryland Energy Innovation Institute".blue)
 let arguments = process.argv
 console.log(arguments)
 if (arguments.length > 2) {
-    filehandler.manipulateFiles(process.argv[2])
+    let foldersCreated = []
+    let mainFunction = async (_) => {
+        await getFolders().then(function (result) {
+            foldersCreated = result
+        })
+        console.log(foldersCreated)
+    }
+    mainFunction()
 } else {
     console.log("Error! No argument was given. (Ex. 'node index.js <filepath to mdat files>)".red)
 }
-
 
